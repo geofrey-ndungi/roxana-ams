873777d913256c83dbf92d5330fffbc5b52b31e9
Jeff-tech67 <geofreytech@yahoo.com>
2026-09-05 20:18:56 +0300

    Added custom User model with role field, configure MySQL

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', #added this coz ill use it at some point
+    'accounts', # added the accounts app
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
+    
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
