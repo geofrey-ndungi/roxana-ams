57d8c234624b171c60e9e43eb1dbdb076b5100c6
Jeff-tech67 <geofreytech@yahoo.com>
2026-09-14 15:23:07 +0300

    Added the urls

--- academics/urls.py
+++ academics/urls.py
@@ -1,7 +1,19 @@
 from rest_framework.routers import DefaultRouter
-from .views import AcademicYearViewSet
+from .views import (AcademicYearViewSet, 
+                    SubjectViewSet,
+                    TermViewSet,
+                    SchoolClassViewSet,
+                    EnrollmentViewSet
+)
+
+
+
 
 router = DefaultRouter()
 router.register(r"academic-years", AcademicYearViewSet)
+router.register(r"subjects", SubjectViewSet)
+router.register(r"terms", TermViewSet)
+router.register(r"school-classes", SchoolClassViewSet)
+router.register(r"enrollments", EnrollmentViewSet)
 
 urlpatterns = router.urls
